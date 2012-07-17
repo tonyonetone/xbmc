8f038a5e2d071be5d0548ea22153b65c420565a3
Chris "Koying" Browet <cbro@semperpax.com>
2012-07-17 19:40:25 +0200

    ADD: JSON-RPC: add a parameter to "Remove" methods allowing to also delete the file

--- xbmc/interfaces/json-rpc/ServiceDescription.h
+++ xbmc/interfaces/json-rpc/ServiceDescription.h
@@ -2583,7 +2583,8 @@ namespace JSONRPC
       "\"transport\": \"Response\","
       "\"permission\": \"RemoveData\","
       "\"params\": ["
-        "{ \"name\": \"movieid\", \"$ref\": \"Library.Id\", \"required\": true }"
+        "{ \"name\": \"movieid\", \"$ref\": \"Library.Id\", \"required\": true },"
+        "{ \"name\": \"deletefile\", \"type\": \"boolean\", \"default\": false, \"description\": \"Whether to delete the physical file\" }"
       "],"
       "\"returns\": \"string\""
     "}",
@@ -2603,7 +2604,8 @@ namespace JSONRPC
       "\"transport\": \"Response\","
       "\"permission\": \"RemoveData\","
       "\"params\": ["
-        "{ \"name\": \"episodeid\", \"$ref\": \"Library.Id\", \"required\": true }"
+        "{ \"name\": \"episodeid\", \"$ref\": \"Library.Id\", \"required\": true },"
+        "{ \"name\": \"deletefile\", \"type\": \"boolean\", \"default\": false, \"description\": \"Whether to delete the physical file\" }"
       "],"
       "\"returns\": \"string\""
     "}",
@@ -2613,7 +2615,8 @@ namespace JSONRPC
       "\"transport\": \"Response\","
       "\"permission\": \"RemoveData\","
       "\"params\": ["
-        "{ \"name\": \"musicvideoid\", \"$ref\": \"Library.Id\", \"required\": true }"
+        "{ \"name\": \"musicvideoid\", \"$ref\": \"Library.Id\", \"required\": true },"
+        "{ \"name\": \"deletefile\", \"type\": \"boolean\", \"default\": false, \"description\": \"Whether to delete the physical file\" }"
       "],"
       "\"returns\": \"string\""
     "}",
