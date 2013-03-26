8bd2896f7257a7177ca0bfe066a4df11e6b60647
Chris "koying" Browet <cbro@semperpax.com>
2013-03-26 18:05:35 +0100

    ADD: [droid] libstagefright dvdplayer hw codec

--- xbmc/android/activity/XBMCApp.h
+++ xbmc/android/activity/XBMCApp.h
@@ -33,6 +33,14 @@
 #include "android/jni/Context.h"
 #include "android/jni/BroadcastReceiver.h"
 
+/*
+#ifdef HAS_LIBSTAGEFRIGHT
+namespace android {
+  class SurfaceTexture;
+}
+#endif
+*/
+
 // forward delares
 class CJNIWakeLock;
 class CAESinkAUDIOTRACK;
@@ -100,6 +108,18 @@ public:
   static int GetMaxSystemVolume();
 
   static int GetDPI();
+  
+#ifdef HAS_LIBSTAGEFRIGHT
+  bool InitStagefrightSurface();
+  void UninitStagefrightSurface();
+  void UpdateStagefrightTexture();
+  void GetStagefrightTransformMatrix(float* transformMatrix);
+
+  ANativeWindow* GetAndroidVideoWindow() const { return m_VideoNativeWindow;}
+  const unsigned int GetAndroidTexture() const { return m_VideoTextureId; }
+  //android::SurfaceTexture* GetSurfaceTexture() const { return m_SurfaceTexture; }
+#endif
+
 protected:
   // limit who can access Volume
   friend class CAESinkAUDIOTRACK;
@@ -122,9 +142,22 @@ private:
   pthread_t m_thread;
   
   static ANativeWindow* m_window;
+#ifdef HAS_LIBSTAGEFRIGHT
+  unsigned int m_VideoTextureId;
+  jobject m_SurfTexture;
+  jobject m_Surface;
+  jmethodID m_midUpdateTexImage;
+  jmethodID m_midGetTransformMatrix;
+  jmethodID midSurfaceTextureRelease;
+  jmethodID midSurfaceRelease;
+  ANativeWindow* m_VideoNativeWindow;
+  //android::SurfaceTexture* m_SurfaceTexture;
+#endif
   
   void XBMC_Pause(bool pause);
   void XBMC_Stop();
   bool XBMC_DestroyDisplay();
   bool XBMC_SetupDisplay();
 };
+
+extern CXBMCApp* g_xbmcapp;
