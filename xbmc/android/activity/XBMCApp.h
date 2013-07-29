eed43384ed8e1b71fc8ddf2ca2bb9f13020c6f0b
Chris "Koying" Browet <cbro@semperpax.com>
2013-07-29 12:21:21 +0200

    FIX: [droid] restore system volume on exit

--- xbmc/android/activity/XBMCApp.h
+++ xbmc/android/activity/XBMCApp.h
@@ -108,6 +108,8 @@ public:
   static bool GetExternalStorage(std::string &path, const std::string &type = "");
   static bool GetStorageUsage(const std::string &path, std::string &usage);
   static int GetMaxSystemVolume();
+  static int GetSystemVolume();
+  static void SetSystemVolume(int val);
 
   static int GetDPI();
   
@@ -139,6 +141,7 @@ private:
   static ANativeActivity *m_activity;
   CJNIWakeLock *m_wakeLock;
   static int m_batteryLevel;  
+  static int m_initialVolume;  
   bool m_firstrun;
   bool m_exiting;
   pthread_t m_thread;
