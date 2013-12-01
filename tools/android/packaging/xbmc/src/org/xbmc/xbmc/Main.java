46a747906534d7da92e4832d8e1a0231bd6ce352
Chris "Koying" Browet <cbro@semperpax.com>
2013-12-01 11:24:27 +0100

    FIX: [droid] catch UnsatisfiedLinkErrors when invoking native

--- tools/android/packaging/xbmc/src/org/xbmc/xbmc/Main.java
+++ tools/android/packaging/xbmc/src/org/xbmc/xbmc/Main.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.util.Log;
 
 public class Main extends NativeActivity 
 {
