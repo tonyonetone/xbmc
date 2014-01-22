a6feb55215493c2aa36cd2a6ddebae853e209547
Chris "Koying" Browet <cbro@semperpax.com>
2014-01-22 13:00:18 +0100

    Revert "FIX: [droid] Splash.java: check sdcard readyness + tweakings"
    This reverts commit 8d7eb14beb9e36874ca64b270506977ca17c7b76.

--- tools/android/packaging/xbmc/src/org/xbmc/xbmc/Splash.java
+++ tools/android/packaging/xbmc/src/org/xbmc/xbmc/Splash.java
@@ -33,24 +33,11 @@ import android.widget.TextView;
 import android.content.res.Resources;
 import android.content.res.Resources.NotFoundException;
 
-import android.os.Handler;
-import android.os.Message;
-import android.content.BroadcastReceiver;
-import android.content.IntentFilter;
-import android.os.Environment;
-
 public class Splash extends Activity {
 
-  private static final int Uninitialized = 0;
-  private static final int InError = 1;
-  private static final int Checking = 2;
-  private static final int ChecksDone = 3;
-  private static final int Clearing = 4;
-  private static final int Caching = 5;
-  private static final int CachingDone = 6;
-  private static final int WaitingStorageChecked = 7;
-  private static final int StorageChecked = 8;
-  private static final int StartingXBMC = 99;
+  public enum State {
+    Uninitialized, InError, Checking, Caching, StartingXBMC
+  }
 
   private static final String TAG = "XBMC";
 
@@ -59,68 +46,13 @@ public class Splash extends Activity {
 
   private ProgressBar mProgress = null;
   private TextView mTextView = null;
-  
-  private int mState = Uninitialized;
+  private State mState = State.Uninitialized;
   public AlertDialog myAlertDialog;
 
   private String sPackagePath;
   private String sApkDir;
   private File fPackagePath;
   private File fApkDir;
-
-  private BroadcastReceiver mExternalStorageReceiver = null;
-  private boolean mExternalStorageChecked = false;
-  private boolean mCachingDone = false;
-
-  private class StateMachine extends Handler {
-
-    private Splash mSplash = null;
-    
-    StateMachine(Splash a) {
-      this.mSplash = a;
-    }
-    
-    @Override
-    public void handleMessage(Message msg) {
-      mSplash.mState = msg.what;
-      switch(mSplash.mState) {
-        case InError:
-          mSplash.finish();
-          break;
-        case Checking:
-          break;
-        case Clearing:
-          mSplash.mTextView.setText("Clearing cache...");
-          mSplash.mProgress.setVisibility(View.INVISIBLE);
-          break;
-        case CachingDone:
-          mSplash.mCachingDone = true;
-          if (mSplash.mExternalStorageChecked)
-            sendEmptyMessage(StartingXBMC);
-          else
-            sendEmptyMessage(WaitingStorageChecked);
-          break;
-        case WaitingStorageChecked:
-          mSplash.mTextView.setText("Waiting for external storage...");
-          mSplash.mProgress.setVisibility(View.INVISIBLE);
-          break;
-        case StorageChecked:
-          mExternalStorageChecked = true;
-          mSplash.stopWatchingExternalStorage();
-          if (mSplash.mCachingDone)
-            sendEmptyMessage(StartingXBMC);
-          break;
-        case StartingXBMC:
-          mSplash.mTextView.setText("Starting XBMC...");
-          mSplash.mProgress.setVisibility(View.INVISIBLE);
-          mSplash.startXBMC();
-          break;
-        default:
-          break;
-      }
-    }
-  }
-  private StateMachine mStateMachine = new StateMachine(this);
 
   public void showErrorDialog(Context context, String title, String message) {
     if (myAlertDialog != null && myAlertDialog.isShowing())
@@ -134,6 +66,7 @@ public class Splash extends Activity {
         new DialogInterface.OnClickListener() {
           public void onClick(DialogInterface dialog, int arg1) {
             dialog.dismiss();
+            finish();
           }
         });
     builder.setCancelable(false);
@@ -145,71 +78,13 @@ public class Splash extends Activity {
         .setMovementMethod(LinkMovementMethod.getInstance());
   }
 
-  private boolean ParseCpuFeature() {
-    ProcessBuilder cmd;
-
-    try {
-      String[] args = { "/system/bin/cat", "/proc/cpuinfo" };
-      cmd = new ProcessBuilder(args);
-
-      Process process = cmd.start();
-      InputStream in = process.getInputStream();
-      byte[] re = new byte[1024];
-      while (in.read(re) != -1) {
-        mCpuinfo = mCpuinfo + new String(re);
-      }
-      in.close();
-    } catch (IOException ex) {
-      ex.printStackTrace();
-      return false;
-    }
-    return true;
-  }
-
-  private boolean CheckCpuFeature(String feat) {
-    final Pattern FeaturePattern = Pattern.compile(":.*?\\s" + feat
-        + "(?:\\s|$)");
-    Matcher m = FeaturePattern.matcher(mCpuinfo);
-    return m.find();
-  }
-
-  void updateExternalStorageState() {
-    String state = Environment.getExternalStorageState();
-    if (Environment.MEDIA_CHECKING.equals(state)) {
-        mExternalStorageChecked = false;
-    } else {
-        mStateMachine.sendEmptyMessage(StorageChecked);
-    }
-  }
-
-  void startWatchingExternalStorage() {
-    mExternalStorageReceiver = new BroadcastReceiver() {
-      @Override
-      public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "Storage: " + intent.getData());
-        updateExternalStorageState();
-      }
-    };
-    IntentFilter filter = new IntentFilter();
-    filter.addAction(Intent.ACTION_MEDIA_MOUNTED);
-    filter.addAction(Intent.ACTION_MEDIA_REMOVED);
-    filter.addAction(Intent.ACTION_MEDIA_SHARED);
-    filter.addAction(Intent.ACTION_MEDIA_UNMOUNTABLE);
-    filter.addAction(Intent.ACTION_MEDIA_UNMOUNTED);
-    registerReceiver(mExternalStorageReceiver, filter);
-  }
-
-  void stopWatchingExternalStorage() {
-    if (mExternalStorageReceiver != null)
-      unregisterReceiver(mExternalStorageReceiver);
-  }
-
-  class FillCache extends AsyncTask<Void, Integer, Integer> {
+  // Do the Work
+  class Work extends AsyncTask<Void, Integer, Integer> {
 
     private Splash mSplash = null;
     private int mProgressStatus = 0;
 
-    public FillCache(Splash splash) {
+    public Work(Splash splash) {
       this.mSplash = splash;
     }
 
@@ -226,7 +101,6 @@ public class Splash extends Activity {
       if (fApkDir.exists()) {
         // Remove existing files
         Log.d(TAG, "Removing existing " + fApkDir.toString());
-        mStateMachine.sendEmptyMessage(Clearing);
         DeleteRecursive(fApkDir);
       }
       fApkDir.mkdirs();
@@ -243,7 +117,7 @@ public class Splash extends Activity {
         mProgress.setProgress(0);
         mProgress.setMax(zip.size());
 
-        mState = Caching;
+        mState = State.Caching;
         publishProgress(mProgressStatus);
         while (entries.hasMoreElements()) {
           // Update the progress bar
@@ -294,7 +168,7 @@ public class Splash extends Activity {
         return -1;
       }
 
-      mState = CachingDone;
+      mState = State.StartingXBMC;
       publishProgress(0);
 
       return 0;
@@ -303,12 +177,17 @@ public class Splash extends Activity {
     @Override
     protected void onProgressUpdate(Integer... values) {
       switch (mState) {
+      case Checking:
+        mSplash.mTextView.setText("Checking package validity...");
+        mSplash.mProgress.setVisibility(View.INVISIBLE);
+        break;
       case Caching:
         mSplash.mTextView.setText("Preparing for first run. Please wait...");
         mSplash.mProgress.setVisibility(View.VISIBLE);
         mSplash.mProgress.setProgress(values[0]);
         break;
-      case CachingDone:
+      case StartingXBMC:
+        mSplash.mTextView.setText("Starting XBMC...");
         mSplash.mProgress.setVisibility(View.INVISIBLE);
         break;
       }
@@ -319,13 +198,41 @@ public class Splash extends Activity {
       super.onPostExecute(result);
       if (result < 0) {
         showErrorDialog(mSplash, "Error", mErrorMsg);
-        mState = InError;
+        return;
       }
 
-      mStateMachine.sendEmptyMessage(mState);
+      startXBMC();
     }
   }
-  
+
+  private boolean ParseCpuFeature() {
+    ProcessBuilder cmd;
+
+    try {
+      String[] args = { "/system/bin/cat", "/proc/cpuinfo" };
+      cmd = new ProcessBuilder(args);
+
+      Process process = cmd.start();
+      InputStream in = process.getInputStream();
+      byte[] re = new byte[1024];
+      while (in.read(re) != -1) {
+        mCpuinfo = mCpuinfo + new String(re);
+      }
+      in.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+
+  private boolean CheckCpuFeature(String feat) {
+    final Pattern FeaturePattern = Pattern.compile(":.*?\\s" + feat
+        + "(?:\\s|$)");
+    Matcher m = FeaturePattern.matcher(mCpuinfo);
+    return m.find();
+  }
+
   protected void startXBMC() {
     // NB: We only preload libxbmc to be able to get info on missing symbols.
     //     This is not normally needed
@@ -355,7 +262,7 @@ public class Splash extends Activity {
         return;
       }
 
-    mStateMachine.sendEmptyMessage(Checking);
+    mState = State.Checking;
 
     String curArch = "";
     try {
@@ -363,10 +270,10 @@ public class Splash extends Activity {
     } catch (IndexOutOfBoundsException e) {
       mErrorMsg = "Error! Unexpected architecture: " + Build.CPU_ABI;
       Log.e(TAG, mErrorMsg);
-      mState = InError;
+      mState = State.InError;
    }
     
-    if (mState != InError) {
+    if (mState != State.InError) {
       // Check if we are on the proper arch
 
       // Read the properties
@@ -379,41 +286,39 @@ public class Splash extends Activity {
         if (!curArch.equalsIgnoreCase(properties.getProperty("native_arch"))) {
           mErrorMsg = "This XBMC package is not compatible with your device (" + curArch + " vs. " + properties.getProperty("native_arch") +").\nPlease check the <a href=\"http://wiki.xbmc.org/index.php?title=XBMC_for_Android_specific_FAQ\">XBMC Android wiki</a> for more information.";
           Log.e(TAG, mErrorMsg);
-          mState = InError;
+          mState = State.InError;
         }
       } catch (NotFoundException e) {
         mErrorMsg = "Cannot find properties file";
         Log.e(TAG, mErrorMsg);
-        mState = InError;
+        mState = State.InError;
       } catch (IOException e) {
         mErrorMsg = "Failed to open properties file";
         Log.e(TAG, mErrorMsg);
-        mState = InError;
+        mState = State.InError;
       }
     }
     
-    if (mState != InError) {
+    if (mState != State.InError) {
       if (curArch.equalsIgnoreCase("arm")) {
         // arm arch: check if the cpu supports neon
         boolean ret = ParseCpuFeature();
         if (!ret) {
           mErrorMsg = "Error! Cannot parse CPU features.";
           Log.e(TAG, mErrorMsg);
-          mState = InError;
+          mState = State.InError;
         } else {
           ret = CheckCpuFeature("neon");
           if (!ret) {
             mErrorMsg = "This XBMC package is not compatible with your device (NEON).\nPlease check the <a href=\"http://wiki.xbmc.org/index.php?title=XBMC_for_Android_specific_FAQ\">XBMC Android wiki</a> for more information.";
             Log.e(TAG, mErrorMsg);
-            mState = InError;
+            mState = State.InError;
           }
         }
       }
     }
     
-    if (mState != InError) {
-      mState = ChecksDone;
-      
+    if (mState != State.InError) {
       sPackagePath = getPackageResourcePath();
       fPackagePath = new File(sPackagePath);
       File fCacheDir = getCacheDir();
@@ -422,34 +327,26 @@ public class Splash extends Activity {
 
       if (fApkDir.exists()
           && fApkDir.lastModified() >= fPackagePath.lastModified()) {
-        mState = CachingDone;
-        mCachingDone = true;
+        mState = State.StartingXBMC;
       }
     }
-    if (!Environment.MEDIA_CHECKING.equals(Environment.getExternalStorageState()))
-      mExternalStorageChecked = true;
 
-    if (mCachingDone && mExternalStorageChecked) {
+    if (mState == State.StartingXBMC) {
       startXBMC();
       return;
     }
-
+    
     setContentView(R.layout.activity_splash);
+
     mProgress = (ProgressBar) findViewById(R.id.progressBar1);
     mTextView = (TextView) findViewById(R.id.textView1);
-
-    if (mState == InError) {
+    
+    if (mState == State.InError) {
       showErrorDialog(this, "Error", mErrorMsg);
       return;
     }
-          
-    if (!mExternalStorageChecked) {
-      startWatchingExternalStorage();
-      mStateMachine.sendEmptyMessage(WaitingStorageChecked);
-    }
 
-    if (!mCachingDone)
-      new FillCache(this).execute();
+    new Work(this).execute();
   }
 
 }
