8a6a269b7f7b3776f9a7e8e9978c3aed2bb54b4f
smallint <tahoma@gmx.de>
2014-02-18 19:19:09 +0100

    Fixed buffer reference and added more logs to track a frames lifetime

--- xbmc/cores/VideoRenderers/LinuxRendererGLES.cpp
+++ xbmc/cores/VideoRenderers/LinuxRendererGLES.cpp
@@ -2738,6 +2738,8 @@ void CLinuxRendererGLES::DeleteIMXMAPTexture(int index)
   if(plane.id && glIsTexture(plane.id))
     glDeleteTextures(1, &plane.id);
   plane.id = 0;
+
+  SAFE_RELEASE(buf.codecinfo);
 }
 bool CLinuxRendererGLES::CreateIMXMAPTexture(int index)
 {
