koying/SPMC

ADD: add a fallback site for internet detection
Somehow, google might be blocked ("Great Wall", firewall, proxy).
Little chance anyone would care to block w3c ;)

xbmc/filesystem/CurlFile.cpp
@@ -878,6 +878,13 @@ bool CCurlFile::IsInternet()
 {
   CStdString strURL = "http://www.google.com";
   bool found = Exists(strURL);
+  if (!found)
+  {
+    // fallback
+    Close();
+    strURL = "http://www.w3.org/";
+    found = Exists(strURL);
+  }
   Close();
 
   return found;

tonyonetone/xbmc

xbmc/filesystem/CurlFile.cpp
@@ -878,6 +878,13 @@ bool CCurlFile::IsInternet()
 {
   CURL url("http://www.google.com");
   bool found = Exists(url);
+  if (!found)
+  {
+    // fallback
+    Close();
+    url.Parse("http://www.w3.org/");
+    found = Exists(url);
+  }
   Close();
 
   return found;