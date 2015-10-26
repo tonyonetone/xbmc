koying/SPMC

ADD: [droid] access individual droid settings from within kodi

xbmc/filesystem/AndroidSettingDirectory.h
@@ -1,0 +1,51 @@
+/*
+ *      Copyright (C) 2012-2013 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+#if defined(TARGET_ANDROID)
+#include "IDirectory.h"
+#include "FileItem.h"
+namespace XFILE
+{
+
+typedef struct
+{
+  const char* name;
+  std::string intent;
+  int sdk;
+} IntentMapping;
+
+class CAndroidSettingDirectory :
+      public IDirectory
+{
+public:
+  CAndroidSettingDirectory(void);
+  virtual ~CAndroidSettingDirectory(void);
+  virtual bool GetDirectory(const CStdString& strPath, CFileItemList &items);
+  virtual bool Exists(const char* strPath) { return true; }
+  virtual bool IsAllowed(const CStdString& strFile) const;
+  virtual DIR_CACHE_TYPE GetCacheType(const CStdString& strPath) const { return DIR_CACHE_NEVER; }
+
+protected:
+  std::vector<IntentMapping> m_intents;
+};
+}
+#endif
+

tonyonetone/xbmc

xbmc/filesystem/AndroidSettingDirectory.h
@@ -1,0 +1,51 @@
+/*
+ *      Copyright (C) 2012-2013 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+#if defined(TARGET_ANDROID)
+#include "IDirectory.h"
+#include "FileItem.h"
+namespace XFILE
+{
+
+typedef struct
+{
+  const char* name;
+  std::string intent;
+  int sdk;
+} IntentMapping;
+
+class CAndroidSettingDirectory :
+      public IDirectory
+{
+public:
+  CAndroidSettingDirectory(void);
+  virtual ~CAndroidSettingDirectory(void);
+  virtual bool GetDirectory(const CURL& url, CFileItemList &items);
+  virtual bool Exists(const char* strPath) { return true; }
+  virtual bool AllowAll() const { return true; }
+  virtual DIR_CACHE_TYPE GetCacheType(const std::string& strPath) const { return DIR_CACHE_NEVER; }
+
+protected:
+  std::vector<IntentMapping> m_intents;
+};
+}
+#endif
+